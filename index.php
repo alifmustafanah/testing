55a4e8a10eb439a930efb02956ab1ebfa544252e
Alif Mustafanah <43715429+alifmustafanah@users.noreply.github.com>
2023-04-07 15:03:49 +0700

    testing
    file upload

---
+++ index.php
@@ -0,0 +1,414 @@
+<?php include "template/header.php";?>
+
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="hero d-flex align-items-center">
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 d-flex flex-column justify-content-center">
+				<h1 data-aos="fade-up">IT Systems</h1>
+				<h2 data-aos="fade-up" data-aos-delay="400">Mi.</h2>
+				<div data-aos="fade-up" data-aos-delay="600">
+					<div class="text-center text-lg-start">
+						<a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+							<i class="bi bi-arrow-right"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
+				<img src="assets/img/hero-img.png" class="img-fluid" alt="">
+			</div>
+		</div>
+	</div>
+
+</section><!-- End Hero -->
+
+<main id="main">
+	<!-- ======= About Section ======= -->
+	<section id="about" class="about">
+
+		<div class="container" data-aos="fade-up">
+			<div class="row gx-0">
+
+				<div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+					<div class="content">
+
+						<div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+							<img src="assets/img/robot.png" class="img-fluid" alt="" style="width: 150px;">
+						</div>
+
+						<h2>MOBOID</h2>
+						<p>
+							Kami adalah sekelompok team yang akan mensupport kelangsungan bisnis anda. dengan system yang mudah dan simple tentunya,kami akan berkontribusi penuh untuk mengembangkan fitur bisnis anda. 
+						</p>
+						<div class="text-center text-lg-start">
+							<a href="#" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
+								<span>Read More</span>
+								<i class="bi bi-arrow-right"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+<div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+							<img src="assets/img/about.jpg" class="img-fluid" alt="">
+						</div>
+
+
+
+			</div>
+		</div>
+
+	</section><!-- End About Section -->
+
+
+
+
+
+	<!-- ======= Portfolio Section ======= -->
+	<section id="portfolio" class="portfolio">
+
+		<div class="container" data-aos="fade-up">
+
+			<header class="section-header">
+				<p>Project</p>
+			</header>
+
+			<div class="row" data-aos="fade-up" data-aos-delay="100">
+				<div class="col-lg-12 d-flex justify-content-center">
+					<ul id="portfolio-flters">
+						<li data-filter="*" class="filter-active">Project </li>
+
+					</ul>
+				</div>
+			</div>
+
+			<div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-app">
+					<div class="portfolio-wrap">
+						<img src="assets/img/portfolio/system_custom.jpg" class="img-fluid" alt="">
+						<div class="portfolio-info">
+							<h4>System Custom Perusahaan</h4>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-web">
+					<div class="portfolio-wrap">
+						<img src="assets/img/portfolio/system_company_profile.jpg" class="img-fluid" alt="">
+						<div class="portfolio-info">
+							<h4>Company Profile</h4>
+
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-app">
+					<div class="portfolio-wrap">
+						<img src="assets/img/portfolio/system_klinik.jpg" class="img-fluid" alt="">
+						<div class="portfolio-info">
+							<h4>System Klinik</h4>
+
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-card">
+					<div class="portfolio-wrap">
+						<img src="assets/img/portfolio/system_logistic.jpg" class="img-fluid" alt="">
+						<div class="portfolio-info">
+							<h4>Software Management Logistic</h4>
+						
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-web">
+					<div class="portfolio-wrap">
+						<img src="assets/img/portfolio/system_lestoran.jpg" class="img-fluid" alt="">
+						<div class="portfolio-info">
+							<h4>System Restourant </h4>
+							
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-app">
+					<div class="portfolio-wrap">
+						<img src="assets/img/portfolio/system_sekolah.jpg" class="img-fluid" alt="">
+						<div class="portfolio-info">
+							<h4>System sekolah</h4>
+						
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-card">
+					<div class="portfolio-wrap">
+						<img src="assets/img/portfolio/system_perdagangan.jpg" class="img-fluid" alt="">
+						<div class="portfolio-info">
+							<h4>System Perdagangan (Business)</h4>
+					
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-card">
+					<div class="portfolio-wrap">
+						<img src="assets/img/portfolio/system_hotel.jpg" class="img-fluid" alt="">
+						<div class="portfolio-info">
+							<h4>System Management Hotel </h4>
+					
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-web">
+					<div class="portfolio-wrap">
+						<img src="assets/img/portfolio/system_kasir.jpg" class="img-fluid" alt="">
+						<div class="portfolio-info">
+							<h4>System Kasir</h4>
+				
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+
+	</section><!-- End Portfolio Section -->
+
+
+
+
+
+	<!-- ======= Pricing Section ======= -->
+	<section id="floworder" class="floworder">
+
+		<div class="container" data-aos="fade-up">
+
+			<header class="section-header">
+				<p>Flow Request</p>
+			</header>
+
+			<div class="row gy-4" data-aos="fade-left">
+
+				<div class="col-lg-12 col-md-6" data-aos="zoom-in" data-aos-delay="100">
+					<div class="box">
+
+						<img src="assets/img/Slide1.JPG" class="img-fluid" alt="">
+
+					</div>
+				</div>
+
+
+
+
+			</div>
+
+		</div>
+
+	</section><!-- End Pricing Section -->
+
+
+
+	<!-- ======= Team Section ======= -->
+	<section id="team" class="team">
+
+		<div class="container" data-aos="fade-up">
+
+			<header class="section-header">
+				<p>Teams</p>
+			</header>
+
+			<div class="row gy-4">
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+					<div class="member">
+						<div class="member-img">
+							<img src="assets/img/team/t1.jpg" class="img-fluid" alt="">
+							<div class="social">
+								<a href=""><i class="bi bi-twitter"></i></a>
+								<a href=""><i class="bi bi-facebook"></i></a>
+								<a href=""><i class="bi bi-instagram"></i></a>
+								<a href=""><i class="bi bi-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Ari Hanggara</h4>
+							<span>CEO</span>
+							<p>Orang yang sulit di kalahkan = orang tidak pernah menyerah</p>
+
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+					<div class="member">
+						<div class="member-img">
+							<img src="assets/img/team/t2.jpg" class="img-fluid" alt="">
+							<div class="social">
+								<a href=""><i class="bi bi-twitter"></i></a>
+								<a href=""><i class="bi bi-facebook"></i></a>
+								<a href=""><i class="bi bi-instagram"></i></a>
+								<a href=""><i class="bi bi-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Dhani</h4>
+							<span>MANAGER</span>
+							<p>Never giveup</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+					<div class="member">
+						<div class="member-img">
+							<img src="assets/img/team/t3.jpg" class="img-fluid" alt="">
+							<div class="social">
+								<a href=""><i class="bi bi-twitter"></i></a>
+								<a href=""><i class="bi bi-facebook"></i></a>
+								<a href=""><i class="bi bi-instagram"></i></a>
+								<a href=""><i class="bi bi-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Aziz</h4>
+							<span>CTO</span>
+							<p>Never giveup</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
+					<div class="member">
+						<div class="member-img">
+							<img src="assets/img/team/t4.jpg" class="img-fluid" alt="">
+							<div class="social">
+								<a href=""><i class="bi bi-twitter"></i></a>
+								<a href=""><i class="bi bi-facebook"></i></a>
+								<a href=""><i class="bi bi-instagram"></i></a>
+								<a href=""><i class="bi bi-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Putra</h4>
+							<span>ENGINEER</span>
+							<p>Never giveup</p>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
+					<div class="member">
+						<div class="member-img">
+							<img src="assets/img/team/ru.jpg" class="img-fluid" alt="">
+							<div class="social">
+								<a href=""><i class="bi bi-twitter"></i></a>
+								<a href=""><i class="bi bi-facebook"></i></a>
+								<a href=""><i class="bi bi-instagram"></i></a>
+								<a href=""><i class="bi bi-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Rully</h4>
+							<span>Backend</span>
+							<p>Never giveup</p>
+						</div>
+					</div>
+				</div>
+				
+				
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
+					<div class="member">
+						<div class="member-img">
+							<img src="assets/img/team/udin.jpg" class="img-fluid" alt="">
+							<div class="social">
+								<a href=""><i class="bi bi-twitter"></i></a>
+								<a href=""><i class="bi bi-facebook"></i></a>
+								<a href=""><i class="bi bi-instagram"></i></a>
+								<a href=""><i class="bi bi-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Syarif</h4>
+							<span>Server</span>
+							<p>Never giveup</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</section><!-- End Team Section -->
+
+
+
+		<!-- ======= Contact Section ======= -->
+		<section id="contact" class="contact">
+
+			<div class="container" data-aos="fade-up">
+
+				<header class="section-header">
+
+					<p>Kontak Kami</p>
+				</header>
+
+				<div class="row gy-4">
+
+					<div class="col-lg-6">
+
+						<div class="row gy-4">
+							<div class="col-md-6">
+								<div class="info-box">
+									<i class="bi bi-geo-alt"></i>
+									<h3>Alamat</h3>
+									<p>Jl rinjani,Blok A no 4 <br>Perumahan Jayasampurna Residence</p>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="info-box">
+									<i class="bi bi-telephone"></i>
+									<h3>Hubungi Kami</h3><br>
+									<p>+6285762226137</p>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="info-box">
+									<i class="bi bi-envelope"></i>
+									<h3>Email Kami</h3>
+									<p>admin@moboid.id</p>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="col-lg-6">
+						<form action="forms/contact.php" method="post" class="php-email-form">
+							<div class="row gy-4">
+
+							<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1089.779235540052!2d107.09402512825172!3d-6.36187389540371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sid!2sid!4v1630298174109!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+								</div>
+
+							</div>
+						</form>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</section><!-- End Contact Section -->
+
+	</main><!-- End #main -->
+
+	<?php include"template/footer.php";?>
+</body>
+
+</html>
